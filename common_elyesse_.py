04beb83a6213191b905c6bc2a1406ae56dd684a2
Elyesse BENYELOUL <elyesse.benyeloul@etu.minesparis.psl.eu>
2024-10-29 15:55:58 +0100

    Fonction pour supprimer les anciennes données

--- common_elyesse_.py
+++ common_elyesse_.py
@@ -6,4 +6,12 @@ fichier = "PS_2024.10.29_06.17.42.csv"
 df = pd.read_csv(fichier, skiprows = 96)
 df.head()
 
+df2 = df.copy()
+df2 = df.drop_duplicates(subset=["pl_name"], keep="last")
+df2.head()
+
+df2["pl_name"].value_counts()
+
+df2.shape
+
 
