98c50102dd948849e5358997119a3fb24d0155f0
Antoine Bossin <antoine.bossin@etu.minesparis.psl.eu>
2024-10-29 15:58:22 +0100

    premier point

--- pe-Antoine-hackathon.py
+++ pe-Antoine-hackathon.py
@@ -1,6 +1,6 @@
 import numpy as np 
 import pandas as pd 
-import matplotlib as plt 
+import matplotlib.pyplot as plt 
 
 
 
@@ -8,6 +8,49 @@ fichier = "PS_2024.10.29_06.17.42.csv"
 
 df = pd.read_csv(fichier, skiprows = 96)
 df.head()
+
+#cohérence types
+df.dtypes
+
+
+
+# +
+#df.describe
+# -
+
+#taille du data set 
+df.shape
+
+# +
+#Bilan valeurs manquantes 
+#df.value_counts()
+# -
+
+#on enlève les colonnes inutiles
+df.drop(["default_flag", "soltype","pl_controv_flag", "pl_refname","ttv_flag"], axis=1, inplace = True)
+df.head()
+
+# +
+#graphe radius = f(orbital period) 
+
+
+plt.scatter(df['pl_orbper'], df['pl_rade'], marker='o')
+
+# Définir l'échelle logarithmique
+plt.xscale('log')
+
+# Ajouter des labels et un titre
+plt.xlabel('Orbital period (days)')
+plt.ylabel('Planet Radius [earth radius]')
+plt.title('Graphique en échelle logarithmique')
+plt.grid(True)
+
+# Afficher le graphique
+plt.show()
+# -
+
+
+
 
 
 
